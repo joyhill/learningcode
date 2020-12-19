d85900c4aac669900ce98ba8d551bc10415d1621
joyhill <76234050+joyhill@users.noreply.github.com>
2020-12-19 13:20:51 -0800

    Create Animals.java

---
+++ Animals.java
@@ -0,0 +1,5 @@
+class Animals {
+  public static void main(String[] args) {
+    System.out.println("Hello little cat! \n \t This is your friend mighty mouse \n We see  a ‘smart squirrel’ in the wood");
+  }
+}
