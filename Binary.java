49d81957ba0ad7cf728d101d591074bef3ab8880
joyhill <76234050+joyhill@users.noreply.github.com>
2020-12-23 22:00:22 -0800

    Create Binary.java

---
+++ Binary.java
@@ -0,0 +1,10 @@
+public class Binary {
+  
+  public static void main(String[] args){
+    int x = -5;
+  
+    System.out.println("binary x = " + Integer.toBinaryString(x));
+    System.out.println("hexadecimal x = " + Integer.toHexString(x));
+  }
+
+}
