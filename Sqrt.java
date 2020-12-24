7022f8bea9a53c4a1c909aca3d3185c4f2378142
joyhill <76234050+joyhill@users.noreply.github.com>
2020-12-23 22:01:59 -0800

    Create Sqrt.java

---
+++ Sqrt.java
@@ -0,0 +1,9 @@
+public class Sqrt {
+  public static void main(String[] args){
+    double input = Double.parseDouble(args[0]);
+    double root = Math.sqrt(input);
+    //remember sqrt math library function 
+    System.out.println("The square root is " + root);
+  }
+
+}
